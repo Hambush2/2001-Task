0dccfa657a97d59fb5dbe9f88479c903175c73ac
Hambush2 <55706777+Hambush2@users.noreply.github.com>
2021-01-24 19:56:45 +0000

    Added program files to repo files
    Didn't initially have the program files within the repo folder, so github did not recognise them. Currently have program connected to the database, as well as methods to access stored proceedures within the database

---
+++ 2001 Web Api/2001 Web Api/Models/DataAccess.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Data.SqlClient;
+
+namespace _2001_Web_Api.Models
+{
+    public class DataAccess : DbContext
+    {
+        private string Connection;
+        private COMP2001_DAndrewsContext context;
+
+        public DataAccess(COMP2001_DAndrewsContext _context)
+        {
+            context = _context;
+        }
+
+        public bool Validate(User user)
+        {
+            if (context.Database.ExecuteSqlRaw("EXEC ValidateUser @email, @password",
+                new SqlParameter("@email", user.Email.ToString()),
+                new SqlParameter("@password", user.Password.ToString())) == 1)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public void Register(User user, string outstring)
+        {
+            context.Database.ExecuteSqlRaw("EXEC ValidateUser @firstName, @surname, @email, @password",
+                new SqlParameter("@firstname", user.Firstname.ToString()),
+                new SqlParameter("@surname", user.Surname.ToString()),
+                new SqlParameter("@email", user.Password.ToString()),
+                new SqlParameter("@password", user.Password.ToString()));
+        }
+
+        public void Update(User user, int id)
+        {
+            context.Database.ExecuteSqlRaw("EXEC UpdateUser @id, @firstName, @surname, @email, @password",
+                new SqlParameter("@id", Convert.ToInt64(user.UserId)),
+                new SqlParameter("@firstname", user.Firstname.ToString()),
+                new SqlParameter("@surname", user.Surname.ToString()),
+                new SqlParameter("@email", user.Password.ToString()),
+                new SqlParameter("@password", user.Password.ToString()));
+        }
+
+        public void Delete(int id)
+        {
+            context.Database.ExecuteSqlRaw("EXEC DeleteUser @id", new SqlParameter("@id", id));
+        }
+    }
+}
---
+++ 2001 Web Api/2001 Web Api/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace _2001_Web_Api.Models
+{
+    public partial class User
+    {
+        public int UserId { get; set; }
+        public string Firstname { get; set; }
+        public string Surname { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
